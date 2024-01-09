26e7309c5e5bf1aaa7f71a384d3124971443fd6e
Vera Smulskaya <vera.smulskaya@gmail.com>
2024-01-09 23:22:07 +0200

    add form to page Movies

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 export default function Movies() {
-  return <div>Movies</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const queryValue = searchParams.get('query');
+  console.log('queryValue', queryValue);
+
+  const onFormSubmit = event => {
+    event.preventDefault();
+    const valueSearch = event.currentTarget.elements.searchKey.value;
+    setSearchParams({ query: valueSearch });
+  };
+
+  return (
+    <div>
+      <form onSubmit={onFormSubmit}>
+        <label>
+          <input
+            type="text"
+            name="searchKey"
+            required
+            placeholder="what movie are you looking for?"
+          />
+        </label>
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 }
