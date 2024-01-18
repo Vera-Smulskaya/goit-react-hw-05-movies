3e20622c95ed9e3b7250fb606b68180b6307db27
Vera Smulskaya <vera.smulskaya@gmail.com>
2024-01-18 22:56:45 +0200

    fix logic in MovieDetails page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from 'components/Layout/Layout';
 import { Suspense, lazy } from 'react';
+import Loader from 'components/Loader/Loader';
 
 const Home = lazy(() => import('pages/Home/Home'));
 const Movies = lazy(() => import('pages/Movies/Movies'));
@@ -13,7 +14,7 @@ export const App = () => {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/movies" element={<Movies />}></Route>
-          <Route path="/movies/:movieId/*" element={<MovieDetailsDetails />} />
+          <Route path="/movies/:movieId/*" element={<MovieDetails />} />
         </Routes>
       </Suspense>
     </Layout>
--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,57 +1,84 @@
-import React, { useEffect, useState } from 'react';
-import { NavLink, Route, Routes, useParams } from 'react-router-dom';
-import axios from 'axios';
+import React, { Suspense, useEffect, useRef, useState } from 'react';
+import {
+  Link,
+  NavLink,
+  Route,
+  Routes,
+  useLocation,
+  useParams,
+} from 'react-router-dom';
 import Loader from 'components/Loader/Loader';
-import MovieCast from '../MovieCast';
+import { fetchMovieDetails } from 'services/api';
+import css from './MovieDetails.module.css';
+import MovieCast from 'components/MovieCast/MovieCast';
+import MovieReviews from 'components/MovieReviews/MovieReviews';
 
 export default function MovieDetails() {
   const { movieId } = useParams();
-  const { movieDetails, setMovieDetails } = useState(null);
+  const { movieDetails, setMovieDetails } = useState('');
   const { isLoading, setIsLoading } = useState(null);
   const { error, setError } = useState(null);
+  const location = useLocation();
+  const backHomePage = useRef(location.state?.from ?? '/');
 
   useEffect(() => {
-    const fetchMovieDetails = async () => {
-      try {
-        setIsLoading(true);
-        const { data } = await axios.get(
-          '/https://api.themoviedb.org/3/trending/movie/day?api_key=7e63573e5f6a11223e01d9dfd6333e93'
-        );
-        setMovieDetails(data);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchMovieDetails();
+    if (!movieId) return;
+    setIsLoading(true);
+    fetchMovieDetails(movieId)
+      .then(result => {
+        setMovieDetails(result);
+      })
+      .catch(setError)
+      .finally(setIsLoading(false));
   }, [movieId]);
 
   return (
     <div>
-      <h1>MovieDetails MovieId: {movieId}</h1>
-      {error !== null && <p>{error}</p>}
+      <Link className={css.backHome} to={backHomePage.current}>
+        Go back
+      </Link>
       {isLoading && <Loader />}
-      {movieDetails !==
-        null(
+      {!error && (
+        <div>
+          <img
+            width={300}
+            alt={movieDetails.title}
+            title={movieDetails.title}
+            src={
+              movieDetails.poster_path
+                ? `https://image.tmdb.org/t/p/w300${movieDetails.poster_path}`
+                : 'No poster found'
+            }
+          />
           <div>
-            <h2>{movieDetails.title}</h2>
-            <h3>{movieDetails.overview}</h3>
-            <h3>{movieDetails.genres}</h3>
+            <h1>{movieDetails.title}</h1>
+            <p>User Score: {Math.round(movieDetails.vote_average * 10)}%</p>
+            <h2>Release date: {movieDetails.release_date.slice(0, 4)}</h2>
+            <h2>Overview</h2>
+            <p>{movieDetails.overview}</p>
+            <h2>Genres</h2>
+            <ul>
+              {movieDetails.genres.map(genre => (
+                <li key={genre.id}>{genre.name}</li>
+              ))}
+            </ul>
           </div>
-        )}
-      <div>
-        <p>Additional information</p>
-        <NavLink className="" to="cast">
-          Cast
-        </NavLink>
-        <Routes>
-          <Route path="cast" element={<MovieCast />} />
-        </Routes>
-        <NavLink className="" to="reviews">
-          Reviews
-        </NavLink>
-      </div>
+          <div>
+            <div>
+              <NavLink to="cast">Cast</NavLink>
+              <NavLink to="reviews">Reviews</NavLink>
+            </div>
+          </div>
+          <div className={css.cast}>
+            <Suspense fallback={<Loader />}>
+              <Routes>
+                <Route path="cast" element={<MovieCast />} />
+                <Route path="reviews" element={<MovieReviews />} />
+              </Routes>
+            </Suspense>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
